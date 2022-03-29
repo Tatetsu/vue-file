772bdeb1c6028a35df028a56c0ac633b14cbe96c
masataka <masataka@masatakanoMacBook-Air.local>
2022-03-29 16:11:49 +0900

    画面を作成しました

--- src/components/NewLetters.vue
+++ src/components/NewLetters.vue
@@ -1,6 +1,102 @@
 <template>
   <div class="main">
-    <img src="../assets/4月.png" alt="">
+    <div class="main_bg" style="position: relative">
+      <h2
+        style="
+          font-size: 1.8em;
+          transform: rotate(-5deg);
+          text-align: left;
+          padding-left: 1rem;
+          margin-bottom: 0;
+        "
+      >
+        しせつからのお便り
+      </h2>
+      <div class="calender">
+        <img
+          src="../assets/calender.png"
+          alt=""
+          style="width: 65px; position: absolute; top: 0; right: 10px"
+        />
+        <p
+          style="
+            color: #fff;
+            position: absolute;
+            top: 0;
+            right: 15px;
+            padding: 0;
+            margin: 0;
+          "
+        >
+          2022年
+          <br style="border-bottom: 1px solid #fff" />
+          <span style="font-size: 2em">4</span>月
+        </p>
+      </div>
+      <div
+        class="user_wright"
+        style="
+          display: flex;
+          flex-direction: row;
+          align-content: flex-start;
+          align-items: flex-start;
+          padding-left: 1rem;
+        "
+      >
+        <div class="user_information">
+          <div class="user_name">
+            <p><input type="text" style="border-radius: 2em" /></p>
+          </div>
+          <div class="user_text">
+            <textarea name="" id="" cols="20" rows="5"></textarea>
+          </div>
+        </div>
+        <div class="user_img">
+          <img
+            src="../assets/4月.png"
+            alt=""
+            style="width: 100%; border-radius: 50%; height: 228px"
+          />
+        </div>
+      </div>
+      <div class="bg_img" style="display: flex; justify-content: space-around">
+        <div class="user_img_bottom" style="height: 250px">
+          <img
+            src="../assets/4月.png"
+            alt=""
+            style="border-radius: 40% 3%; width: 45%; height: 100%"
+          />
+          <img
+            src="../assets/4月.png"
+            alt=""
+            style="border-radius: 50%; width: 45%; height: 100%"
+          />
+        </div>
+      </div>
+      <div class="user_comment" style="position: relative; margin-top: 2rem">
+        <h3
+          style="
+            position: absolute;
+            top: -15px;
+            right: calc(100% - 75%);
+            margin: 0 auto;
+            border-radius: 1rem;
+            padding: 1% 5%;
+            background-color: blue;
+            font-size: 1em;
+            color: #fff;
+          "
+        >
+          施設からのコメント
+        </h3>
+        <div class="user_comment_input" style="margin-bottom: 2rem;">
+          <textarea name="" id="" cols="40" rows="5" style="padding: 5%"></textarea>
+        </div>
+      </div>
+    </div>
+    <div class="download">
+      <button>ダウンロード</button>
+    </div>
   </div>
 </template>
 
@@ -12,8 +108,15 @@ export default {
 </script>
 
 <style scoped>
-.main img{
-min-width: 400px;
-min-height: 840px;
+img {
+  width: 100%;
+}
+.main {
+  /* height: 100vh; */
+  background-color: bisque;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
